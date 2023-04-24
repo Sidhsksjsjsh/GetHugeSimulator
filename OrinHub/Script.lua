42eacf1446eb1b6e4a51b4a7eebff30827a949d4
Terminated <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-04-25 02:19:34 +0900

    Update and rename .gitignore to OrinHub/Script.lua

---
+++ OrinHub/Script.lua
@@ -0,0 +1,269 @@
+local EV
+
+getgenv().Egg = false;
+getgenv().Buy = false;
+getgenv().Lift = false;
+getgenv().Sell = false;
+getgenv().Attack = false;
+getgenv().Territory = false;
+
+function Open()
+    spawn(function ()
+        while getgenv().Egg == true do
+if EV == "Creature Egg" then local args = {[1] = 1,[2] = 1,[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+if EV == "Wildlife Egg" then local args = {[1] = 2,[2] = 1,[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+if EV == "Xtended Egg" then local args = {[1] = 3,[2] = 1,[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+if EV == "Emerald Egg" then local args = {[1] = 4,[2] = 1,[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+if EV == "Heaven's Egg" then local args = {[1] = 5,[2] = 1,[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+if EV == "Santa's Egg" then local args = {[1] = 1,[2] = 1,[3] = "Christmas"} game:GetService("ReplicatedStorage").Remotes.Pets.PurchaseEgg:InvokeServer(unpack(args)) wait(0.1) end
+        end
+    end)
+end
+function Byer()
+spawn(function () while getgenv().Buy == true do
+local args = {[1] = "Weight",[2] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestBuyAll:InvokeServer(unpack(args)) wait(0.5)
+        end
+    end)
+spawn(function () while getgenv().Buy == true do
+local args = {[1] = 2,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 3,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 4,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 5,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 6,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 7,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 8,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 9,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 10,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 11,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 12,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 13,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 14,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 15,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+local args = {[1] = 16,[2] = "BodyUpgrade",[3] = "Islands"} game:GetService("ReplicatedStorage").Remotes.Shop.RequestPurchase:InvokeServer(unpack(args)) wait(1)
+        end
+    end)
+end
+function Lift()
+    spawn(function ()
+        while getgenv().Lift == true do
+game:GetService("ReplicatedStorage").Remotes.LiftWeight:FireServer() wait(0.1)
+        end
+    end)
+end
+function Sell()
+    spawn(function ()
+        while getgenv().Sell == true do
+game:GetService("ReplicatedStorage").Remotes.SellStrengthRequest:FireServer() wait(0.1)
+        end
+    end)
+end
+function Attack()
+    spawn(function ()
+        while getgenv().Attack == true do
+local args = {[1] = "Punch",[2] = 2} game:GetService("ReplicatedStorage").Remotes.PVP.AttackAttempt:FireServer(unpack(args)) wait(0.1)
+        end
+    end)
+end
+function Territorial()
+    spawn(function ()
+        while getgenv().Territory == true do
+TPTo(game:GetService("Workspace").RingAreas.Territories.T1.CFrame) wait(10)
+TPTo(game:GetService("Workspace").RingAreas.Territories.T2.CFrame) wait(10)
+TPTo(game:GetService("Workspace").RingAreas.Territories.T3.CFrame) wait(10)
+TPTo(game:GetService("Workspace").RingAreas.Territories.T4.CFrame) wait(10)
+TPTo(game:GetService("Workspace").RingAreas.Territories.T5.CFrame) wait(10)
+        end
+    end)
+end
+
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/Uub92rmN"))()
+
+
+local Window = library:AddWindow("Orin - Cheat",
+    {
+        main_color = Color3.fromRGB(0, 128, 0),
+        min_size = Vector2.new(373, 433),
+        toggle_key = Enum.KeyCode.RightShift,
+    })
+local b = Window:AddTab("AutoFarm")
+
+b:AddSwitch("Auto Lift", function(val)
+getgenv().Lift = val
+Lift()
+end)
+
+b:AddSwitch("Auto upgrade", function(val)
+getgenv().Buy = val
+Byer()
+end)
+
+b:AddSwitch("Auto sell", function(val)
+getgenv().Sell = val
+Sell()
+end)
+
+b:AddSwitch("Auto Attack", function(val)
+getgenv().Attack = val
+Attack()
+end)
+
+b:AddSwitch("Capture Territory", function(val)
+getgenv().Territory = val
+Territorial()
+end)
+
+local EggListTable = b:AddDropdown("Select egg", function(val)
+EV = val
+end)
+
+EggListTable:Add("Creature Egg")
+EggListTable:Add("Wildlife Egg")
+EggListTable:Add("Xtended Egg")
+EggListTable:Add("Emerald Egg")
+EggListTable:Add("Heaven's Egg")
+EggListTable:Add("Santa's Egg")
+
+b:AddSwitch("Auto Hatch", function(val)
+getgenv().Egg = val
+Open()
+end)
+
+local c = Window:AddTab("Miscellaneous")
+
+C:AddButton("Redeem Codes", function()
+local args = {[1] = "russo"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "gaming_dan"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "tofuu"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "workoutisland"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "gravy"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "mortadela"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "BuffNoobJr"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "SynxChazz"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "jackedtiger"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "bicep"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "CAPTAINNOOB"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "Summer"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "NoobBot"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "MadGorilla"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "Swords"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+local args = {[1] = "MAD GORILLA"} game:GetService("ReplicatedStorage").Remotes.ProcessPromoCode:InvokeServer(unpack(args)) wait()
+end) 
+
+local dv = Window:AddTab("Player")
+
+local S
+local J
+local H
+local G
+ 
+dv:AddTextBox("Speed", function(val)
+S = val
+end)
+
+dv:AddTextBox("Jump", function(val)
+J = val
+end)
+
+dv:AddTextBox("Hip Height", function(val)
+H = val
+end)
+
+dv:AddTextBox("Gravity", function(val)
+G = val
+end)
+ 
+dv:AddSwitch("Set speed", function(bool)
+getgenv().Speed = bool
+Speed(S)
+end)
+
+dv:AddSwitch("Set jump", function(bool)
+getgenv().Jump = bool
+Jump(J)
+end)
+
+dv:AddSwitch("Set hip height", function(bool)
+getgenv().Hip = bool
+Hip(H)
+end)
+
+dv:AddSwitch("Set gravity", function(bool)
+getgenv().Grav = bool
+Grav(G)
+end) 
+ 
+dv:AddButton("Full LocalPlayer", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/GphZf9Xe"))()
+end)
+ 
+ 
+function TPTo(PCFrame)
+    local plr = game.Players.LocalPlayer;
+           if plr.Character then
+            plr.Character.HumanoidRootPart.CFrame = PCFrame
+        end
+    end
+ 
+getgenv().Speed = false;
+getgenv().Jump = false;
+getgenv().Hip = false;
+getgenv().Chat = false;
+getgenv().Grav = false;
+ 
+function Chat(Mes, Freq)
+    spawn(function ()
+        while getgenv().Chat == true do
+local args = {[1] = Mes,[2] = "All"} game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(Freq)
+        end
+    end)
+end
+ 
+function Speed(Nume)
+    spawn(function ()
+        if getgenv().Speed == true then
+            while getgenv().Speed == true do
+                game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = Nume
+                wait()
+            end
+        elseif getgenv().Speed == false then
+            game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        end
+    end)
+end
+function Jump(Nume)
+    spawn(function ()
+        if getgenv().Jump == true then
+            while getgenv().Jump == true do
+                game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = Nume
+                wait()
+        end
+    elseif getgenv().Jump == false then
+            game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = 50
+        end
+    end)
+end
+function Hip(Nume)
+    spawn(function ()
+        if getgenv().Hip == true then
+            while getgenv().Hip == true do
+            game:GetService("Players").LocalPlayer.Character.Humanoid.HipHeight = Nume
+            wait()
+        end
+    elseif getgenv().Hip == false then
+            game:GetService("Players").LocalPlayer.Character.Humanoid.HipHeight = 0.1
+        end
+    end)
+end
+function Grav(Nume)
+    spawn(function ()
+        if getgenv().Grav == true then
+            while getgenv().Grav == true do
+                game:GetService("Workspace").Gravity = Nume
+            wait()
+        end
+    elseif getgenv().Grav == false then
+        game:GetService("Workspace").Gravity = 196.2
+        end
+    end)
+end
